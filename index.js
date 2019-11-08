30ddbf97552d6098b39c9c45bf5f2e5261014cca
unknown <jarrettphale@gmail.com>
2019-11-08 16:18:32 -0500

    routes middleware/db connection

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 require('dotenv').config();
 
-const port = process.env.PORT;
 const app = express();
-const quotes = express.Router();
+const port = process.env.PORT;
 
-app.listen(port);
-console.log(`App listening on ${port}`);
+function connectToDB() {
+  mongoose.connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'IASIPQuotes'
+  });
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log('connected');
+  });
+}
+
+connectToDB();
+app.use('/', routes);
+
+app.get('/api/:character', (req, res) => {
+  const character = req.params.character;
+
+  mongoose.connection.db.collection(
+    'IASIPQuotes',
+    collection.find(
+      { character: character }.toArray(function(err, results) {
+        console.log(results);
+      })
+    )
+  );
+
+  // quote.find({ character: character }, (err, iter) => {
+  //   if (err) return console.log(err);
+  //   console.log(iter);
+  // });
+  res.status(200).json('connected');
+});
+
+app.listen(port, () => console.log(`App listening on ${port}`));
